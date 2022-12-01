91c244f20b8ee37200def23815d65bf9bd02ac54
robertIsaac <dev.robert.isaac@gmail.com>
2022-12-01 19:05:34 +0200

    add karma file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'add-angular15-missing-files';
+
+  testCoverage() {
+    // Coverage for functions should fail with this
+  }
 }
